78b564579fc79d0a063be44e201294ddac7a524a
Mikhail Tulkin <mikhail@tulkin.name>
2016-02-11 20:54:52 +0500

    no message

--- current/db_module.h
+++ current/db_module.h
@@ -42,7 +42,7 @@ class DBModule {
 
   // intepreter - program
   virtual int startProgram(int uniq_index) = 0;
-  virtual DBRobotData *makeChoise(DBRobotData** robots_data, unsigned int count_robots) = 0;
+  virtual const DBRobotData *makeChoise(const DBRobotData** robots_data, unsigned int count_robots) = 0;
   virtual int endProgram(int uniq_index) = 0;
 
   // destructor
